327757d78b0ffdeaeb9ea567d4706af42a8c6b28
PrasathRvg <prasathrvg@gmail.com>
2021-09-04 16:45:17 +0530

    first commit

---
+++ money-manager/forecastj.js
@@ -0,0 +1,23 @@
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e)=>{
+ let arrowParent = e.target.parentElement.parentElement; 
+ arrowParent.classList.toggle("showMenu");
+  });
+}
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
+});
+
+function fn1() {
+  var rd1 = document.getElementById('rd1');
+  var rd2 = document.getElementById('rd2');
+
+  if (rd1.checked == true)
+    window.open('frdaybr.html',"_self");
+  else if (rd2.checked == true)
+    window.open('frbarchart.html',"_self");
+}
